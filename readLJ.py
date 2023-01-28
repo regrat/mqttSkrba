b8f44506f970b948bcf3c8953064dbe95d728ae3
Boris Domajnko <51022375+regrat@users.noreply.github.com>
2023-01-28 18:41:02 +0100

    exception for arso

--- readLJ.py
+++ readLJ.py
@@ -1,23 +1,25 @@
-import bs4 as bs
+mport bs4 as bs
 import urllib.request
 
-# vrne trenutno temperaturo
+# current temperatue
 
 def getTemp():
     url = "https://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/observationAms_si_latest.html"
-    source = urllib.request.urlopen(url).read()
-    soup = bs.BeautifulSoup(source,features="html.parser")
-    table = soup.find('table')
-    table_rows = table.find_all('tr')
     out = ""
-    for tr in table_rows:
-        td = tr.find_all('td')
-        row = [i.text for i  in td]
-        if len(row) > 0 and row[0] == "Ljubljana":
-            #print(row[0], row[2])
-            out = row[2]
-            break
+    try:
+        source = urllib.request.urlopen(url).read()
+        soup = bs.BeautifulSoup(source,features="html.parser")
+        table = soup.find('table')
+        table_rows = table.find_all('tr')
+        for tr in table_rows:
+            td = tr.find_all('td')
+            row = [i.text for i  in td]
+            if len(row) > 0 and row[0] == "Ljubljana":
+                #print(row[0], row[2])
+                out = row[2]
+                break
+    except Exception as err:
+        print(f"readLJ {err} {type(err)}")
     return(out)
 
 # print(getTemp())
-
