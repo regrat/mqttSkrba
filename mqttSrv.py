a60a98c9abc740871cdb7809063d2d9fb91c76c4
BorisD <boris.domajnko@telemach.net>
2024-01-14 11:23:52 +0100

    mqtt folder

--- mqttSrv.py
+++ mqttSrv.py
@@ -22,8 +22,8 @@ import paho.mqtt.client as mqtt
 import sqlite3
 import time, sys
 
-import readTemp as B18
-import readLJ as LJ
+import mqtt.readTemp as B18
+import mqtt.readLJ as LJ
 import srvWebMqtt.meritve as M
 import socket
 
