6ea5a0c77101a38c03072495acee80b10286a871
Boris Domajnko <boris.domajnko@telemach.net>
2026-01-25 14:27:28 +0100

    Napacno branje tabele ppm

--- mqtt/readLJ.py
+++ mqtt/readLJ.py
@@ -2,7 +2,7 @@ import bs4 as bs
 import urllib.request
 
 # current temperature and humidity
-# 1.11.2025
+# 25.1.2026
 
 def getTemp():
     url = "https://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/observationAms_si_latest.html"
@@ -37,16 +37,17 @@ def getTemp():
             source = urllib.request.urlopen(url).read()
             soup = bs.BeautifulSoup(source,features="html.parser")
             tables = soup.find_all('table', class_="online")
-            for table in tables:
-                table_rows = table.find_all('tr')
-                for tr in table_rows:
-                    td = tr.find_all('td')
-                    row = [i.text for i  in td]
-                    if len(row) > 0 and row[0] == "LJ Vič":
-                        print(row[0], row[1], row[2])
-                        outP10 = row[1]
-                        outP25 = row[2]
-                        break
+            #for table in tables:
+            table = tables[0]
+            table_rows = table.find_all('tr')
+            for tr in table_rows:
+                td = tr.find_all('td')
+                row = [i.text for i  in td]
+                if len(row) > 0 and row[0] == "LJ Vič":
+                    print(row[0], row[1], row[2])
+                    outP10 = row[1]
+                    outP25 = row[2]
+                    break
         except Exception as err:
             print(f"readLJ {err} {type(err)}")
 
